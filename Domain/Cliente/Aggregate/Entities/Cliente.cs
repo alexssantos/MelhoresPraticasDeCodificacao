c06240c4350f453809e3f3bd68fc5d07246eade7
Alex Santos <arkanjo.alex@gmail.com>
2020-09-21 19:40:56 -0300

    ValueObject CPF

--- Domain/Cliente/Aggregate/Entities/Cliente.cs
+++ Domain/Cliente/Aggregate/Entities/Cliente.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Cliente.Aggregate.ValueObjects;
+using System;
 using System.Collections.Generic;
 
 namespace Domain.Cliente.Aggregate.Entities
@@ -7,8 +8,12 @@ namespace Domain.Cliente.Aggregate.Entities
 	{
 		public string Nome { get; set; }
 		public DateTime DataNscimento { get; set; }
+		public CPF CPF { get; set; }
 		public string NomePai { get; set; }
 		public string NomeMae { get; set; }
+		public string LoginCliente { get; set; }
+		public string SenhaCliente { get; set; }
+		public string Email { get; set; }
 		public IList<Endereco> Enderecos { get; set; }
 		public IList<Telefone> Telefones { get; set; }
 	}
---
+++ Domain/Cliente/Aggregate/ValueObjects/CPF.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Domain.Cliente.Aggregate.ValueObjects
+{
+	public class CPF : Shared.ValueObject
+	{
+		public string Valor { get; set; }
+
+		public CPF()
+		{
+
+		}
+
+		public CPF(string valor)
+		{
+			this.Valor = valor?.Replace(".", "").Replace("-", "") ?? throw new ArgumentException(nameof(CPF));
+		}
+
+		private string ValorFormatado(string valor) => Convert.ToInt64(valor).ToString(@"000\.000\.000\-00");
+	}
+}
